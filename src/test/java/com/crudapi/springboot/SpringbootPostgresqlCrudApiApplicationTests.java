8d3e5cd0e0b02d661f2c3c7c83c0b5e3de03c03f
abhishekgoyal-oyo <abhishek.goyal1@oyorooms.com>
2021-02-09 15:48:29 +0530

    Initialize springboot project

---
+++ src/test/java/com/crudapi/springboot/SpringbootPostgresqlCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudapi.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgresqlCrudApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
